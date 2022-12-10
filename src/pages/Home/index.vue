74dffc0e894aaaa0283f11c2c307dc0ea5e75d9b
linguanghui <1476465526@qq.com>
2022-12-10 22:23:34 +0800

    feat: pinia 全局状态管理

--- src/pages/Home/index.vue
+++ src/pages/Home/index.vue
@@ -1,3 +1,10 @@
 <template>
     Home page
+    <button @click="user.add()">count: {{ user.count }}</button>
 </template>
+
+<script setup>
+
+import { useUserStore } from "@/stores/user";
+const user = useUserStore()
+</script>
